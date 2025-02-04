304ff02f547d952e4cb2da3ed2e331369ec335e3
datlogarit <doandathchh@gmail.com>
2025-02-04 21:44:14 +0700

    add back end of project

---
+++ backend/jobporal/src/test/java/com/project/jobporal/JobporalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jobporal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobporalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
